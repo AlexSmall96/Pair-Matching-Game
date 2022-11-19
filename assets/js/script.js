11c881cf567087fda2639d6080e84b5806a78637
AlexSmall96 <alex.small739@gmail.com>
2022-11-19 14:22:56 +0000

    "Add turn over functionality to items found secion"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let playButton = document.getElementById("play");
+    let playButton = document.getElementById("new-game");
     playButton.addEventListener("click", runGame);
-
-    let cards = document.getElementsByClassName('card');
-
-    for (let card of cards){
-        card.addEventListener('click', function(){
-            let itemName=this.id;
-            card.innerHTML=`<i class="fa-2xl fa-solid fa-${itemName}"></i>`
-            console.log(itemName, card.innerHTML)
-        });
-    }
 });
 
 function runGame(){
-  console.log('Game running')
+    let cards = document.getElementsByClassName('card');
+    let input=[];
+
+    for (let card of cards){
+        card.addEventListener('click', function(){
+            if (input.length<2){
+               let itemName=this.id;
+               card.innerHTML=`<i class="fa-2xl fa-solid fa-${itemName}"></i>`;
+               input.push(itemName);
+               if (input[0]===input[1]){
+                let foundItem = document.getElementById(`found-${itemName}`)
+                foundItem.innerHTML=`<i class="fa-2xl fa-solid fa-${itemName}"></i>`;
+               }
+            }
+        });
+    }
 }
 
 function showCard(item){
