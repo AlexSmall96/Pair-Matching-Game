e504b57c43b185e923fe6ad8647e5d2ff0d11efd
AlexSmall96 <alex.small739@gmail.com>
2022-11-19 16:39:12 +0000

    "Add score counter"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
+    //Add event listener to play button//
     let playButton = document.getElementById("new-game");
-    playButton.addEventListener("click", runGame);
-});
-
-function runGame(){
+    playButton.addEventListener("click", newGame);
+    //Add event listeners to cards//
     let cards = document.getElementsByClassName('card');
     let input=[];
-
+    let itemsFound=0;
     for (let card of cards){
         card.addEventListener('click', function(){
+            //If less than two cards are currently chosen//
             if (input.length<2){
                let itemName=this.id;
+               //Turn card over to show item//
                card.innerHTML=`<i class="fa-2xl fa-solid fa-${itemName}"></i>`;
                input.push(itemName);
+               //If cards selected match
                if (input[0]===input[1]){
+                //Add item to found items//
                 let foundItem = document.getElementById(`found-${itemName}`)
                 foundItem.innerHTML=`<i class="fa-2xl fa-solid fa-${itemName}"></i>`;
+                itemsFound++;
+                console.log(itemsFound);
+               }
+               if (input.length === 2){
+                //If two attempts made reset input to empty array//
+                  input =[];
                }
             }
         });
     }
+});
+
+function newGame(){ 
+    //turns all cards over
+    //resets score
+    //outputs empty array and score of 0
 }
 
-function showCard(item){
-    console.log(item ,'selected');
-}
 function compareItems(item1,item2){
     
 }
