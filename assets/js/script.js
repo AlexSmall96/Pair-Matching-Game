ac1d8ff086a08e4acdc0547c56943232dc06c5fd
AlexSmall96 <alex.small739@gmail.com>
2022-11-19 13:00:45 +0000

    "Add event listeners to cards and play button"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
-function runGame(){
+document.addEventListener("DOMContentLoaded", function() {
+    let playButton = document.getElementById("play");
+    playButton.addEventListener("click", runGame);
 
+    let cards = document.getElementsByClassName('card');
+
+    for (let card of cards){
+        card.addEventListener('click', showCard);
+    }
+});
+
+function runGame(){
+  console.log('Game running')
 }
 
+function showCard(){
+    console.log('Card selected');
+}
 function compareItems(item1,item2){
     
 }
