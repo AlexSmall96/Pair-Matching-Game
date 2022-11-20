7f13af92441038c5b6dc641b601c8d1eec621dd2
AlexSmall96 <alex.small739@gmail.com>
2022-11-20 21:11:33 +0000

    "Add Guess Again button and create event listener for it"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     //Add event listener to play button//
+    let input=[];
+    let itemsFound=0;
     let playButton = document.getElementById("new-game");
     playButton.addEventListener("click", newGame);
     //Add event listeners to cards//
     let cards = document.getElementsByClassName('card');
-    let input=[];
-    let itemsFound=0;
     for (let card of cards){
         card.addEventListener('click', function(){
             //If less than two cards are currently chosen//
@@ -23,13 +23,26 @@ document.addEventListener("DOMContentLoaded", function() {
                   itemsFound++;
                   }
                 }
-               if (input.length === 2){
-                //If two attempts made reset input to empty array//
-                  input =[];
-               }
             }
         });
     }
+
+    let guessAgain=document.getElementById('guess-again');
+    guessAgain.addEventListener('click',function(){
+        //Check if two cards have been selected//
+        if (input.length ===2){
+            console.log('2')
+            //Check if cards dont match//
+            if (input[0]!==input[1]){
+                console.log('wrong')
+                //Turn cards back over//
+                document.getElementById(input[0]).innerHTML=
+                `<i class="fa-2xl fa-solid fa-question"></i>`;
+                document.getElementById(input[1]).innerHTML=
+                `<i class="fa-2xl fa-solid fa-question"></i>`;
+            }
+        }
+    })
 });
 
 function newGame(){ 
