a2d78e81a82478505744bfd2a82c82aef1b380cc
AlexSmall96 <alex.small739@gmail.com>
2022-12-07 17:34:56 +0000

    "Define difficulty variables within game button event listener"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let noAttemptsEasy=25;
-    let noAttemptsHard=50;
-    let attemptsLeft=noAttemptsEasy;
     let difficulty='easy';
     let inputId=[];
     let inputName=[];
     let itemsFound=0;
-    let gameInProgress=false;
     let scores={' ':' ',' ':' ',' ':' ' };
     const homePage=document.getElementById('home-page');
     const gamePage=document.getElementById('game-page');
     const leaderboardRows=document.getElementById('leaderboard').getElementsByTagName('tr');
     const medalOrder = {1:"gold" ,2:"silver",3:"bronze"};
     const diffMultiplier={'easy':1,'hard':1.5};
-    let cards = document.getElementsByClassName('card');
-    //Define object of items to map number to card id
-    const keysHard=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-    const itemsHard= {
-            1:"pizza-slice-0",
-            2:"pizza-slice-1",
-            3:"mug-hot-0",
-            4:"mug-hot-1",
-            5:"burger-0",
-            6:"burger-1",
-            7:"cat-0",
-            8:"cat-1",
-            9:"motorcycle-0",
-            10:"motorcycle-1",
-            11:"cake-candles-0",
-            12:"cake-candles-1",
-            13:"ice-cream-0",
-            14:"ice-cream-1",
-            15:"snowman-0",
-            16:"snowman-1",
-            17:"shuttle-space-0",
-            18:"shuttle-space-1",
-            19:"carrot-0",
-            20:"carrot-1"
-            };  
-    //When easy mode is chosen
-    const keysEasy=[1,2,3,4,5,6,7,8,9,10,11,12];
-    const itemsEasy={
-            1:"pizza-slice-0",
-            2:"pizza-slice-1",
-            3:"mug-hot-0",
-            4:"mug-hot-1",
-            5:"burger-0",
-            6:"burger-1",
-            7:"cat-0",
-            8:"cat-1",
-            9:"motorcycle-0",
-            10:"motorcycle-1",
-            11:"cake-candles-0",
-            12:"cake-candles-1"
-            };  
-    
+    const promptArea=document.getElementById('prompt-area');
+    const playButton = document.getElementById("new-game");
+    const exitButton = document.getElementById("exit-game");
+    //Define variables that change with difficulty
+    let items;
+    let keys;
+    let noAttempts;
     //Add event listener to difficulty switch
     const diffSwitch=document.getElementById('switch');
     diffSwitch.addEventListener('change', function(){
         if (this.checked){
-            difficulty='hard';
-            if (difficulty==='hard'){
-                let items=itemsHard;
-                let keys=keysHard;
-                let noAttempts=noAttemptsHard;
-                document.getElementById('attempts-left').innerHTML = noAttempts;
+                difficulty='hard';
                 //If hard mode is chosen change card layout
                 let gameTable=document.getElementById('game-table');
                 gameTable.innerHTML=
@@ -97,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 <td id="carrot-1" class="card"><i class="fa-2xl fa-solid fa-question"></i></td>
                 <td id="burger-0" class="card"><i class="fa-2xl fa-solid fa-question"></i></td>
             </tr>
-                `
+                `;
             let foundTable=document.getElementById('found-table');
             foundTable.innerHTML=
             `
@@ -116,9 +72,8 @@ document.addEventListener("DOMContentLoaded", function() {
             <td id="found-snowman"><i class="fa-2xl fa-solid fa-question"></i></td>
             </tr>
             `;
-            cards = document.getElementsByClassName('card'); 
-            let noItems=cards.length*0.5;
-            } 
+            
+             
         } else {difficulty='easy';
           let gameTable=document.getElementById('game-table');
           gameTable.innerHTML=
@@ -142,10 +97,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 <td id="pizza-slice-0" class="card"><i class="fa-2xl fa-solid fa-question"></i></td>
             </tr>
           `;
-        items=itemsEasy;
-        keys=keysEasy;
-        noAttempts=noAttemptsEasy;
-        document.getElementById('attempts-left').innerHTML = noAttempts;
         let foundTable=document.getElementById('found-table');
         foundTable.innerHTML=
         `
@@ -160,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function() {
         <td id="found-cake-candles"><i class="fa-2xl fa-solid fa-question"></i></td>
         </tr>
         `;
-        cards = document.getElementsByClassName('card'); 
+        
         
     }
     });
@@ -168,23 +119,55 @@ document.addEventListener("DOMContentLoaded", function() {
    
 
 
-    const promptArea=document.getElementById('prompt-area');
-  
-    
+   
 
-  
-
-    const playButton = document.getElementById("new-game");
+   
     playButton.addEventListener("click", function(){
     if (difficulty==='easy'){
-        items=itemsEasy;
+        items={
+            1:"pizza-slice-0",
+            2:"pizza-slice-1",
+            3:"mug-hot-0",
+            4:"mug-hot-1",
+            5:"burger-0",
+            6:"burger-1",
+            7:"cat-0",
+            8:"cat-1",
+            9:"motorcycle-0",
+            10:"motorcycle-1",
+            11:"cake-candles-0",
+            12:"cake-candles-1"
+            };
         keys=[1,2,3,4,5,6,7,8,9,10,11,12];
-        noAttempts=noAttemptsEasy;
+        noAttempts=25;
     } else {
-        items=itemsHard;
+        items={
+            1:"pizza-slice-0",
+            2:"pizza-slice-1",
+            3:"mug-hot-0",
+            4:"mug-hot-1",
+            5:"burger-0",
+            6:"burger-1",
+            7:"cat-0",
+            8:"cat-1",
+            9:"motorcycle-0",
+            10:"motorcycle-1",
+            11:"cake-candles-0",
+            12:"cake-candles-1",
+            13:"ice-cream-0",
+            14:"ice-cream-1",
+            15:"snowman-0",
+            16:"snowman-1",
+            17:"shuttle-space-0",
+            18:"shuttle-space-1",
+            19:"carrot-0",
+            20:"carrot-1"
+            };
         keys=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-        noAttempts=noAttemptsHard;
+        noAttempts=50;
+        
     }
+    document.getElementById('attempts-left').innerHTML = noAttempts;
     gamePage.style.display='block';
     homePage.style.display='none';
     //Shuffle keys into new array//
@@ -202,7 +185,7 @@ document.addEventListener("DOMContentLoaded", function() {
     keys=shuffledKeys;
     let cardNo=0;
     let oldId;
-    cards = document.getElementsByClassName('card'); 
+    let cards = document.getElementsByClassName('card'); 
     let noItems=keys.length*0.5;
     for (let card of cards){
         //Turn all cards over
@@ -218,19 +201,16 @@ document.addEventListener("DOMContentLoaded", function() {
         }     
         itemsFound=0;
         //Reset attempts left
-        attemptsLeft=noAttempts;
+        let attemptsLeft=noAttempts;
         document.getElementById('attempts-left').innerHTML = attemptsLeft;
         inputId=[];
         inputName=[];
         itemsFound=0;
         promptArea.innerHTML=`<p>Pick a Card!</p>`;
-        gameInProgress=true;
         let username=document.getElementById('username').value;
         //Add event listeners to cards//
     for (let card of cards){
         card.addEventListener('click', function(){
-            //Only let the user click if New game button has been clicked//
-            if (gameInProgress){
             //Only let the user click if attempts are left
             if (attemptsLeft>0){
             //Only let the user click on a card if it hasn't been turned over//
@@ -263,7 +243,6 @@ document.addEventListener("DOMContentLoaded", function() {
                     promptArea.innerHTML=`<p>Well done! 1 item left to find.</p>`;
                   } else {
                     promptArea.innerHTML=`<p>Well done! All items found.</p>`;
-                    gameInProgress=false;
                     
                     //Update users score if new high score is achieved and username was entered
                     if (username){
@@ -292,7 +271,6 @@ document.addEventListener("DOMContentLoaded", function() {
                   inputId=[];
                   inputName=[];
                   } else if (attemptsLeft<noItems-itemsFound){
-                    gameInProgress=false;
                     promptArea.innerHTML='<p>Sorry, not enough guesses remaining.</p>';
                   } else if (attemptsLeft>0){
                     //Let button appear to guess again//
@@ -318,20 +296,16 @@ document.addEventListener("DOMContentLoaded", function() {
                        
                     });
                   } else {promptArea.innerHTML='<p>Sorry, you ran out of guesses.</p>';
-                          gameInProgress=false;
                 }
                 }
             }
         }
         }
-        }
+        
         });
     }
     });
 
-    
-    
-    const exitButton = document.getElementById("exit-game");
     exitButton.addEventListener("click", function(){
     gamePage.style.display='none';
     homePage.style.display='block';
